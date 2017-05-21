fff6d8b785114f80810bee528d8b9effb5a3ebbb
fuerbringer <severin@protonmail.ch>
2017-05-21 16:25:40 +0200

    Added first prototype database

---
+++ install/adminapi.sql
@@ -0,0 +1,238 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: db
+-- Generation Time: May 21, 2017 at 02:23 PM
+-- Server version: 10.1.22-MariaDB-1~jessie
+-- PHP Version: 7.0.16
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `adminapi`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customers`
+--
+
+CREATE TABLE `customers` (
+  `customer_id` int(11) NOT NULL,
+  `gender` enum('m','f') NOT NULL,
+  `firstname` varchar(256) NOT NULL,
+  `lastname` varchar(256) NOT NULL,
+  `birthday` date NOT NULL,
+  `email` varchar(256) NOT NULL,
+  `mobile` varchar(64) NOT NULL,
+  `telephone` varchar(64) NOT NULL,
+  `country` varchar(256) NOT NULL,
+  `state` varchar(256) NOT NULL,
+  `postal` varchar(16) NOT NULL,
+  `address` varchar(256) NOT NULL,
+  `company` varchar(256) NOT NULL,
+  `uid` varchar(512) NOT NULL COMMENT 'Unternehmens-Identifikationsnummer'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `employee`
+--
+
+CREATE TABLE `employee` (
+  `employee_id` int(11) NOT NULL,
+  `firstname` varchar(64) NOT NULL,
+  `lastname` varchar(64) NOT NULL,
+  `birthdate` date NOT NULL,
+  `date_hired` date NOT NULL,
+  `active` tinyint(1) NOT NULL DEFAULT '1',
+  `employee_role_id` int(11) NOT NULL,
+  `photo_url` varchar(512) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `employee_role`
+--
+
+CREATE TABLE `employee_role` (
+  `employee_role_id` int(11) NOT NULL,
+  `name_en` varchar(64) NOT NULL,
+  `name_de` varchar(64) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `employee_role`
+--
+
+INSERT INTO `employee_role` (`employee_role_id`, `name_en`, `name_de`) VALUES
+(1, 'Software Engineer', 'Applikationsentwickler'),
+(2, 'Entrepreneur', 'Geschäftsleiter');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product`
+--
+
+CREATE TABLE `product` (
+  `product_id` int(11) NOT NULL,
+  `product_type_id` int(11) NOT NULL,
+  `customer_id` int(11) NOT NULL,
+  `existence` enum('new','existing') NOT NULL,
+  `domain` varchar(256) NOT NULL,
+  `support` tinyint(1) NOT NULL,
+  `supported_platforms` varchar(256) NOT NULL,
+  `description` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product_type`
+--
+
+CREATE TABLE `product_type` (
+  `produkt_typ_id` int(11) NOT NULL,
+  `name_en` varchar(64) NOT NULL,
+  `name_de` varchar(64) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `product_type`
+--
+
+INSERT INTO `product_type` (`produkt_typ_id`, `name_en`, `name_de`) VALUES
+(1, 'Website', 'Webseite'),
+(2, 'Mobile App', 'Mobile App'),
+(3, 'Web Application', 'Web Applikation'),
+(4, 'Desktop Application', 'Desktop Applikation');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product_type_detail`
+--
+
+CREATE TABLE `product_type_detail` (
+  `product_type_detail_id` int(11) NOT NULL,
+  `product_type_id` int(11) NOT NULL,
+  `name` varchar(64) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `protocol`
+--
+
+CREATE TABLE `protocol` (
+  `protocol_id` int(11) NOT NULL,
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `product_id` int(11) NOT NULL,
+  `employee_id` int(11) NOT NULL,
+  `protocol_text` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `customers`
+--
+ALTER TABLE `customers`
+  ADD PRIMARY KEY (`customer_id`);
+
+--
+-- Indexes for table `employee`
+--
+ALTER TABLE `employee`
+  ADD PRIMARY KEY (`employee_id`);
+
+--
+-- Indexes for table `employee_role`
+--
+ALTER TABLE `employee_role`
+  ADD PRIMARY KEY (`employee_role_id`);
+
+--
+-- Indexes for table `product`
+--
+ALTER TABLE `product`
+  ADD PRIMARY KEY (`product_id`);
+
+--
+-- Indexes for table `product_type`
+--
+ALTER TABLE `product_type`
+  ADD PRIMARY KEY (`produkt_typ_id`);
+
+--
+-- Indexes for table `product_type_detail`
+--
+ALTER TABLE `product_type_detail`
+  ADD PRIMARY KEY (`product_type_detail_id`);
+
+--
+-- Indexes for table `protocol`
+--
+ALTER TABLE `protocol`
+  ADD PRIMARY KEY (`protocol_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `customers`
+--
+ALTER TABLE `customers`
+  MODIFY `customer_id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `employee`
+--
+ALTER TABLE `employee`
+  MODIFY `employee_id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `employee_role`
+--
+ALTER TABLE `employee_role`
+  MODIFY `employee_role_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT for table `product`
+--
+ALTER TABLE `product`
+  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `product_type`
+--
+ALTER TABLE `product_type`
+  MODIFY `produkt_typ_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+--
+-- AUTO_INCREMENT for table `product_type_detail`
+--
+ALTER TABLE `product_type_detail`
+  MODIFY `product_type_detail_id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `protocol`
+--
+ALTER TABLE `protocol`
+  MODIFY `protocol_id` int(11) NOT NULL AUTO_INCREMENT;COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
