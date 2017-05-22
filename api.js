15c58d513676d3a1e5f630be4f0c83998ef268cd
fuerbringer <severin@protonmail.ch>
2017-05-22 19:50:19 +0200

    Created basic programming structures

---
+++ api.js
@@ -0,0 +1,10 @@
+var jsonfile = require('jsonfile')
+var rest = require('./libs/rest')
+
+jsonfile.readFile('config/auth.json', (err, params) => {
+	if(err) {
+		console.error('Whoops! Authentication file `auth.json` could not be found. \nMake sure to copy the example file like this: `cp config/auth.json.example auth.json`.\n\n')
+	} else {
+		rest.go(params)
+	}
+})
---
+++ libs/rest.js
@@ -0,0 +1,9 @@
+var db = require('./db')
+
+module.exports = {
+	go: function(params) {
+		db.initialize(params.db_host, params.db_name, params.db_user, params.db_pass, () => {
+			// Provide RESTful API calls here...
+		})
+	}
+}
